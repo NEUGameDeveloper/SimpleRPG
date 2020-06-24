9fc6ff32b408328064cc62bbe43ecb2f19f52431
ZhangDi <di.zhang@centurygame.com>
2020-06-24 16:01:17 +0800

    初始化工程

---
+++ Client/Assets/Scripts/Framework/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Singleton<T> where T : Singleton<T>
+{
+    private static T s_Instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (s_Instance == null)
+            {
+                s_Instance = Activator.CreateInstance<T>();
+                s_Instance.OnInit();
+            }
+            return s_Instance;
+        }
+    }
+
+    public static void DestroyInstance()
+    {
+        if (s_Instance != null)
+        {
+            s_Instance.OnDeInit();
+            s_Instance = null;
+        }
+    }
+
+    protected virtual void OnInit()
+    {
+    }
+
+    protected virtual void OnDeInit()
+    {
+    }
+}
---
+++ Client/Assets/Scripts/Logic/Main.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Main : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
