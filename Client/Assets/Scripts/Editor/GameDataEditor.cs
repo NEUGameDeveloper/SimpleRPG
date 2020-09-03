76f0f6941a28f86cd984748348a4fb83c2aeb589
ZhangDi <di.zhang@centurygame.com>
2020-09-03 14:50:44 +0800

    调整工程结构

---
+++ Client/Assets/Scripts/Editor/GameDataEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector.Editor;
+
+namespace RPG.Editor
+{
+    public class GameDataEditor : OdinMenuEditorWindow
+    {
+        protected override OdinMenuTree BuildMenuTree()
+        {
+            return null;
+        }
+    }
+}
